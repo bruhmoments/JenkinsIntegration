685debab5e4b8502898738791f119ab503d3e80a
bruhmoments <manbigboss7@gmail.com>
2020-01-07 11:51:04 +0700

    Jenkins 2

--- Number.java
+++ Number.java
@@ -1,8 +1,21 @@
 public class Number {
     public static void main(String args[]) {
       System.out.println("Hello World");
-        for (int i=0; i<10; i++){
-            System.out.println(i);
+        for (int i=1; i<=100; i++){
+            if(cPrima(i)) {
+            	System.out.println(i);	
+            }
+            
         }
+    }
+    public static boolean cPrima(int nCek) {
+        int nRem;
+        for (int i = 2; i <= nCek / 2; i++) {
+            nRem = nCek % i;
+            if (nRem == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
